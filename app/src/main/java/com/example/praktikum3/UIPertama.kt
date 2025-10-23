f2b22cc0fe12dd3159452a83e777a2100cea7299
ZahwaAnritia <anritia15@gmail.com>
2025-10-23 15:57:11 +0700

    tambahkan import untuk komponen Material3 dan fungsi Composable

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -2,3 +2,7 @@ package com.example.praktikum3
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
