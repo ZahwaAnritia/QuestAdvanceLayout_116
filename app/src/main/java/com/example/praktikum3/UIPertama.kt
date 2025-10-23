cd7ddc916b1237aac5059f42f218e0fcaa92c9ec
ZahwaAnritia <anritia15@gmail.com>
2025-10-23 16:04:43 +0700

    tambahkan jarak vertikal antara teks dan Card dengan Spacer 25dp

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -33,6 +33,7 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(25.dp))
 
     }
 }
