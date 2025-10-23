e1012cd60356a5c5496cbb20672d188cb043cbab
ZahwaAnritia <anritia15@gmail.com>
2025-10-23 16:13:12 +0700

    tambahkan Box di bagian bawah untuk teks copyright

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -62,11 +62,19 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                     modifier = Modifier.padding(top = 10.dp)
                 )
             }
-
-
-
         }
-
     }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(id = R.string.copy),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 50.dp)
+        )
+    }
+
 }
 
