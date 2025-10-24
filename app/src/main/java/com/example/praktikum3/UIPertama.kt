91186e03399c53e8e693eb0472600927bab1d493
ZahwaAnritia <anritia15@gmail.com>
2025-10-24 12:05:29 +0700

    Tambah teks nama di dalam Column

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -89,6 +89,13 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                 )
                 Spacer(modifier = Modifier.width(30.dp))
                 Column{
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
 
                 }
             }
